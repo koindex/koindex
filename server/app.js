e4f9d425fdeb5a5ce46552aaf5d264028a99140b
YUXUAN YANG <Y464YANG@uwaterloo.ca>
2018-05-21 14:21:13 -0400

    redis module setup

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@
  */
 
 'use strict';
-import Sequelize from 'sequelize';
 import express from 'express';
 import sqldb from './sqldb';
 import config from './config/environment';
@@ -12,6 +11,7 @@ import initWebSocketServer from './config/websockets';
 import expressConfig from './config/express';
 import registerRoutes from './routes';
 import seedDatabaseIfNeeded from './config/seed';
+import redis from './components/redis';
 
 
 // Setup server
@@ -24,6 +24,7 @@ registerRoutes(app);
 // Start server
 function startServer() {
     testDB();
+    testRedis();
     app.angularFullstack = server.listen(config.port, config.ip, function() {
         console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
     });
@@ -38,6 +39,15 @@ function testDB() {
     })
     .catch(err => {
         console.error('Unable to connect to the database:', err);
+    });
+}
+//Test Redis connection
+function testRedis(){
+    redis.set('test', 'test1', function(err, reply) {
+        console.log(reply);
+    });
+    redis.get('test', function(err, reply) {
+        console.log(reply);
     });
 }
 
