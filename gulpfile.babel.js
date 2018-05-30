a3a3d7eb2bce573d6322ccc3901ea6df0c6dcc34
Yang Leng <y7leng@edu.uwaterloo.ca>
2018-05-30 00:33:39 -0400

    Fix gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -324,7 +324,7 @@ gulp.task('serve', cb => {
             'copy:fonts:dev',
             'env:all'
         ],
-        // 'webpack:dev',
+        'webpack:dev',
         ['start:server', 'start:client'],
         'watch',
         cb
